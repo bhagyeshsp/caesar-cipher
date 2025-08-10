4759beb144a10b29990ef91eb1c9527f2cfe0b4d
Bhagyesh Pathak <bhagyeshspathak@gmail.com>
2025-08-10 21:46:46 +0530

    completed all tests

--- spec/cipher_decipher_spec.rb
+++ spec/cipher_decipher_spec.rb
@@ -14,6 +14,14 @@ describe "#caesar_cipher" do
   # No need to test it
   # ! Although I just noticed that it outputs the last-run method's return value
   # ! For our purposes, we will just test it in the last method and treat this method as a pure Public Script method
+  # ? Plan change: as I wrote the other tests and passed, I realized I can test this method too
+  # ? Now I realized that my cipher implementation doesn't retain the capital letters
+  it "returns a cipher string based on the input string and a shift factor" do
+    string_to_cipher = "What a string!"
+    shift_factor = 5
+    result = "bmfy f xywnsl!"
+    expect(caesar_cipher(string_to_cipher, shift_factor)).to eq(result)
+  end
 end
 
 describe "#string_to_array" do
@@ -39,13 +47,20 @@ end
 describe "#shift_right" do
   # This is a Public Query method
   # Need to test its return value
-  xit "generates a shifted array by adding the shift factor to each number" do
+  it "generates a shifted array by adding the shift factor to each number" do
+    input_array = [49, 8, 1, 20, " ", 1, " ", 19, 20, 18, 9, 14, 7, "!"]
+    shift_factor = 5
+    shifted_array = [2, 13, 6, 25, " ", 6, " ", 24, 25, 23, 14, 19, 12, "!"]
+    expect(shift_right(input_array, shift_factor)).to eq(shifted_array)
   end
 end
 
 describe "#shifted_char" do
   # This is a Public Query method
   # Need to test its return value
-  xit "returns a character array mapped from a provided shifted number array" do
+  it "returns a character array mapped from a provided shifted number array" do
+    input_array = [2, 13, 6, 25, " ", 6, " ", 24, 25, 23, 14, 19, 12, "!"]
+    shifted_char_array = ["b", "m", "f", "y", " ", "f", " ", "x", "y", "w", "n", "s", "l", "!"]
+    expect(shifted_char(input_array)).to eq(shifted_char_array)
   end
 end
